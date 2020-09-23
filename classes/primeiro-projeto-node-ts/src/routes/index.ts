1716e5dad77d1081a8bb4838706fa6344ae40a21
Ramon Menezes <xramonx@gmail.com>
2020-09-23 05:39:20 -0300

    EmptyProject OK - Config  vscode debug, yarn, EditorConfig, ESLint, Prettier
    add use.json () to processs the body info.
    Configured debugger for vscode, yarn, EditorConfig, ESLint, Prettier.
    Splitted server from routes/index correctly.

--- classes/primeiro-projeto-node-ts/src/routes/index.ts
+++ classes/primeiro-projeto-node-ts/src/routes/index.ts
@@ -2,4 +2,15 @@
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.post('/users', (request, response) => {
+  const { name, email } = request.body;
+
+  const user = { name, email };
+  // return response.json({ message: 'Hello World' });
+  // const today = new Date();
+  // return response.json(`ᓚᘏᗢ Hello World - ${today.getTime()}`);
+  return response.json(user);
+});
+
 export default routes;
--- classes/primeiro-projeto-node-ts/src/server.ts
+++ classes/primeiro-projeto-node-ts/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-// src/server.ts
 import routes from './routes';
 
 const app = express();
-app.get('/', (request, response) => {
-  const today = new Date();
-  return response.json(`ᓚᘏᗢ Hello World - ${today.getTime()}`);
-});
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('ᓚᘏᗢ Server started on port 3333!');
